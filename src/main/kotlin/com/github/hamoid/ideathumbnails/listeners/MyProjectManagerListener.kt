98dcf150e72b8789d439c4c35cb40f7e4915e5b6
Abe Pazos <abe@hamoid.com>
2021-03-23 08:59:43 +0100

    Add comments, rename

--- src/main/kotlin/com/github/hamoid/ideathumbnails/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/hamoid/ideathumbnails/listeners/MyProjectManagerListener.kt
@@ -10,4 +10,20 @@ internal class MyProjectManagerListener : ProjectManagerListener {
     override fun projectOpened(project: Project) {
         project.service<MyProjectService>()
     }
+
+    /*
+    // https://plugins.jetbrains.com/docs/intellij/psi-files.html
+    To find files with a specific name anywhere in the project, use
+    FilenameIndex.getFilesByName(project, name, scope)
+     */
+
+    /*
+    // This demo seems useful
+    // https://github.com/JetBrains/intellij-sdk-code-samples/tree/main/project_view_pane
+
+    describes an implementation of the com.intellij.projectViewPane extension
+    point, which allows creating an additional presentation type for the
+    Project view pane. ImagesProjectViewPane limits the project tree to
+    the images only.
+    */
 }
