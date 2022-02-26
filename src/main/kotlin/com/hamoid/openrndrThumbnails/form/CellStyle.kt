a8c127eb8aa26d4b443bc4a94bd9a5a43a6b3da9
Abe Pazos <abe@hamoid.com>
2022-02-26 17:38:08 +0100

    Rename ImageListCellRenderer.kt

---
+++ src/main/kotlin/com/hamoid/openrndrThumbnails/form/CellStyle.kt
@@ -0,0 +1,33 @@
+package com.hamoid.openrndrThumbnails.form
+
+import com.intellij.ui.JBColor
+import java.awt.Component
+import javax.swing.*
+
+/**
+ * Changes the cell rendering style depending on if it is selected or not.
+ */
+class ImageListCellRenderer : ListCellRenderer<Any> {
+    override fun getListCellRendererComponent(
+        list: JList<out Any>?,
+        value: Any?,
+        index: Int,
+        isSelected: Boolean,
+        cellHasFocus: Boolean
+    ): Component =
+        if (value is JPanel) {
+            value.components.forEach {
+                it.foreground = if (isSelected) {
+                    UIManager.getColor("List.selectionForeground")
+                } else {
+                    JBColor.foreground()
+                }
+            }
+            value.background = if (isSelected)
+                UIManager.getColor("List.selectionBackground")
+            else
+                JBColor.background()
+            value
+        }
+        else JLabel("")
+}
